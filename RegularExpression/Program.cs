600b990c50167887788436b56d9c21dfe769ee19
mohdhanif1234 <hanif.topia@gmail.com>
2021-09-24 12:05:19 +0530

    [Hanif]:Added a program for first name validation

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexPatternMatching firstName = new RegexPatternMatching();
+            firstName.ValidateFirstName("Hanif");
+        }
+    }
+}
---
+++ RegularExpression/RegexPatternMatching.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    class RegexPatternMatching
+    {
+        // UC-1: First name starts with a capital letter and has minimum 3 letters followed by it
+
+        public string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{3,4}$";
+
+        public void ValidateFirstName(string fName)
+        {
+            if (Regex.IsMatch(fName, FIRST_NAME_REGEX))
+            {
+                Console.WriteLine("Pattern is valid");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Pattern is invalid");
+                Console.ReadLine();
+            }
+        }
+    }
+}
